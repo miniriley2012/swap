acb52ca1690d4bd48334fc4d1d0ac80e13072991
Christian Flewelling <cflew@tutanota.com>
2021-09-08 18:50:50 -0500

    Forgot to clean one thing up
    __NR_renameat2 instead of renameat2 (im not even sure if this is a real
    thing)

--- src/swap.c
+++ src/swap.c
@@ -21,7 +21,7 @@
 int swap_names(int first_fd, const char *first_path, int second_fd, const char *second_path) {
 #if defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12
     return renameatx_np(first_fd, first_path, second_fd, second_path, RENAME_SWAP);
-#elif defined(renameat2)
+#elif defined(__NR_renameat2)
     return renameat2(first_fd, first_path, second_fd, second_path, RENAME_EXCHANGE);
 #elif defined(syscall)
     return syscall(SYS_renameat2, first_fd, first_path, second_fd, second_path, RENAME_EXCHANGE);
